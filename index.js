bcd1d0dc27c5570097fca656973194619c4245b2
T-TNThien <nhatthien0525@gmail.com>
2024-11-25 22:25:55 +0700

    upload all file

---
+++ index.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById('submitButton');
+const a = document.getElementById('answerA');
+const b = document.getElementById('answerB');
+const c = document.getElementById('answerC');
+const d = document.getElementById('answerD');
+
+submit.onclick = function () {
+  
+}
